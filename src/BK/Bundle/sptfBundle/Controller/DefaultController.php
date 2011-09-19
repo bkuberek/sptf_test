611ca03cdc40be8e47b367c50ad6b120e8981425
Bastian Kuberek <bastian@bkuberek.com>
2011-09-19 07:31:41 -0400

    wrapping the frontend test. I haven't slept yet.

--- src/BK/Bundle/sptfBundle/Controller/DefaultController.php
+++ src/BK/Bundle/sptfBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace BK\Bundle\sptfBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,34 @@ class DefaultController extends Controller
   public function indexAction()
   {
     return $this->render('BKsptfBundle:Default:index.html.twig', array());
+  }
+  
+  /**
+   * @Route("/try-premium", name="try_premium")
+   */
+  public function tryAction()
+  {
+    return $this->render('BKsptfBundle:Default:try.html.twig', array());
+  }
+  
+  /**
+   * @Route("/user/validate", name="user_validate")
+   */
+  public function validateAction()
+  {
+    $field = $this->get('request')->get('field');
+    $value = $this->get('request')->get('value');
+    
+    $result = array('valid' => true);
+    
+    if ($field == 'user_email') {
+      $result['valid'] = false;
+      $result['message'] = 'Already taken';
+    }
+    
+    $response = new Response(json_encode($result));
+    $response->headers->set('Content-Type', 'application/json');
+    return $response;
   }
 
 }
--- web/js/app.js
+++ web/js/app.js
@@ -1,40 +1,116 @@
 
+
+var User = User || {};
+
+User.validate = function(key, value) {
+    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+    var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+    var weakRegex = new RegExp("(?=.{6,}).*", "g");
+    
+    var response = {};
+    
+    switch (this.id) {
+        case 'user_username':
+        case 'user_email':
+            $.get("/user/validate", {field: key, value: value}, function(data){
+                response = data;
+            });
+            break;
+        case 'user_passord':
+            if (false == weakRegex.test(value)) {
+                response.valid = true;
+                response.message = 'Weak!'
+            } else if (strongRegex.test(value)) {
+                response.valid = true;
+                response.message = 'Strong!'
+            } else if (mediumRegex.test(value)) {
+                response.valid = true;
+                response.message = 'Medium!'
+            }
+            break;
+    }
+    
+    return response;
+};
+
 $(document).ready(function() {
+    
+    var decache = Math.random() * 999999999999;
+    /**
+     * Try Spotify Premium
+     */
+    $('.spotify-free-try-btn').colorbox({
+        inline: false,
+        opacity: 0.8,
+        innerWidth: '645px',
+        scrolling: false,
+        href: '/try-premium?null='+decache,
+        onComplete: function() {
+            // could do some form validation here
+            $('#colorbox form').bind('submit', function(event) {
+                event.preventDefault();
+                
+                
+                return false;
+            });
+            
+            // could do some form field validation here...
+            $('#colorbox form input').bind('keyup', function(event) {
+                if (this.value.length < 3) {
+                    this.valid = false;
+                    return;
+                }
+                var response = User.validate(this.id, this.value);
+                var msg;
+                if (response.valid) {
+                    msg = 'ok.'
+                } else {
+                    msg = 'bad.'
+                }
+                
+                if (response.message) {
+                    msg += ' '+ response.message;
+                }
+                
+                $(this).next('.notification').text(msg)
+            });
+        }
+    });
    
-   // IMDB filter by range form
-   $('#filter-range').bind('submit', function(event) {
-       event.preventDefault();
-       var url = $(this).attr('data-action');
-       var start = this.start.value;
-       var end = this.end.value;
+    // IMDB filter by range form
+    $('#filter-range').bind('submit', function(event) {
+        event.preventDefault();
+        var url = $(this).attr('data-action');
+        var start = this.start.value;
+        var end = this.end.value;
        
-       if (url.substring(-1) != '/') {
-           url += '/';
-       }
+        if (url.substring(-1) != '/') {
+            url += '/';
+        }
        
-       if (!(!isNaN(parseFloat(start)) && isFinite(start))) {
-           alert('Invalid starting year');
-           return false;
-       } else if (start.length != 4) {
-           alert('Starting year must have 4 digits');
-           return false;
-       }
+        if (!(!isNaN(parseFloat(start)) && isFinite(start))) {
+            alert('Invalid starting year');
+            return false;
+        } else if (start.length != 4) {
+            alert('Starting year must have 4 digits');
+            return false;
+        }
        
-       url += start;
+        url += start;
        
-       if (end) {
-           if (!(!isNaN(parseFloat(end)) && isFinite(end))) {
+        if (end) {
+            if (!(!isNaN(parseFloat(end)) && isFinite(end))) {
                 alert('Invalid ending year');
                 return false;
             } else if (end.length != 4) {
                 alert('Ending year must have 4 digits');
                 return false;
             }
-           url += '-' + end;
-       }
+            url += '-' + end;
+        }
  
-       window.location.href = url;
+        window.location.href = url;
 
-       return false;
-   })
+        return false;
+    })
 });
